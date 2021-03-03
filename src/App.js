9a86a952e16d1fc20172cf3216afedf21bbb22d3
bitewolf <2189108@gmail.com>
2021-03-03 17:24:45 +0300

    49

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import DialogsContainer from './components/Dialogs/DialogsContainer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
+import Users from './components/Users/Users'
+import UsersContainer from './components/Users/UsersContainer'
 import Music from './Music'
 import News from './News'
 import Settings from './Settings'
@@ -21,6 +23,7 @@ const App = () => {
         <div className='app-wrapper-content'>
           <Route path='/dialogs' render={() => <DialogsContainer/>}/>
           <Route path='/profile' render={() => <Profile/>}/>
+          <Route path='/users' render={() => <UsersContainer/>}/>
           <Route path='/news' render={() => <News/>}/>
           <Route path='/music' render={() => <Music/>}/>
           <Route path='/settings' render={() => <Settings/>}/>
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './users.module.css'
+
+let Users = (props) => {
+    if (props.users.length === 0) {
+        props.setUsers ( [
+        {id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4KWIKwaojpneN3qgoL7Ec2xT4EcwjbQ8ImQ&usqp=CAU', followed: false, fullName:'Dmitry', status: 'I am boss', location: {city: 'Minsk', country: 'BLR'}},
+        {id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4KWIKwaojpneN3qgoL7Ec2xT4EcwjbQ8ImQ&usqp=CAU', followed: true, fullName:'Andrey', status: 'I am boss 2', location: {city: 'Braslav', country: 'BLR'}},
+        {id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4KWIKwaojpneN3qgoL7Ec2xT4EcwjbQ8ImQ&usqp=CAU', followed: false, fullName:'Petya', status: 'I am boss 3', location: {city: 'Piter', country: 'RU'}},
+        {id: 4, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4KWIKwaojpneN3qgoL7Ec2xT4EcwjbQ8ImQ&usqp=CAU', followed: true, fullName:'Vitaly', status: 'I am boss 4', location: {city: 'Vinnica', country: 'UA'}}
+    
+        ])
+    }
+    
+    
+    return <div>
+        {
+            props.users.map( u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photoUrl} className={styles.usersPhoto}/>
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollowed</button>
+                            : <button onClick={() => {props.follow(u.id)}}>Followed</button>}
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.fullName}</div><div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.city}</div><div>{u.location.country}</div>
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
+}
+
+export default Users

7ac496e6036d99391ddfc95dbb3e1ad8916bc348
bitewolf <2189108@gmail.com>
2021-03-05 17:10:11 +0300

    52+

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,6 +23,9 @@ const Navbar = (props) => {
                 <div className={s.item}>
                     <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
                 </div>
+                <div className={s.item}>
+                    <NavLink to="/users" activeClassName={s.active}>Users</NavLink>
+                </div>
             </nav>
             {/* <Sidebar /> */}
         </div>

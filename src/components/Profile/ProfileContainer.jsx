e12731cdebf1aaffada112d6d720380ac774d7d7
bitewolf <2189108@gmail.com>
2021-03-07 19:02:40 +0300

    60

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,10 +3,19 @@ import { setUserProfile } from '../../redux/profile-reducer'
 import React from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class ProfileContainer extends React.Component {
+
+  
+
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+
+    let userId = this.props.match.params.userId
+    if (!userId) {
+      userId = 2
+    }
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then (response => {
                 this.props.setUserProfile(response.data)
             })
@@ -23,4 +32,6 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile}) (ProfileContainer)
+let withUrlDataContainerComponent = withRouter (ProfileContainer)
+
+export default connect(mapStateToProps, {setUserProfile}) (withUrlDataContainerComponent)
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,15 +5,15 @@ const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
-
-    return (
+   return (
         <div>
             <div>
             <img src='https://drifterplanet.com/wp-content/uploads/2017/12/Cancun-Private-Beach-Party.jpg'></img>
             </div>
             <div>
-                <img src={props.profile.photos.large}></img>
-            ava + description
+                <img src={props.profile.photos.large}></img><br></br>
+                <span>{props.profile.aboutMe}</span><br></br>
+                <span>{props.profile.fullName}</span>
             </div>
         </div>
     )

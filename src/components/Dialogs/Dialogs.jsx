46da4357f39c7325a98ecfb56b34fcde02ba9261
bitewolf <2189108@gmail.com>
2021-03-17 21:50:41 +0300

    Messages Formik

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,57 +3,51 @@ import DialogItem from './DialogItem/DialogItem'
 import s from './Dialogs.module.css'
 import Message from './Message/Message'
 import React from 'react'
-import { Field, Form, FormikProvider, useFormik } from 'formik'
+import { useFormik } from 'formik'
 
 const Dialogs = (props) => {
     let state = props.messagesPage
     let dialogsElements = state.dialogsData.map ( d => <DialogItem name={d.name} key={d.id} id={d.id} />)
     let messagesElements = state.messagesData.map ( s => <Message message={s.message} key={s.id}/>)
-    // let newMessageElement = React.createRef()
     let addMessage = () => {
         props.sendMessage()
     }
-    // let onNewMessageChange = () => {
-    //     let newMessageText = newMessageElement.current.value
-    //     props.updatenewMessageTextCreator(newMessageText)
-    // }
+
 
     const formik = useFormik({
         initialValues: {
             message: 'new message'
         },
         onSubmit: values => {
-            let newMessageText = values.message
-            props.updatenewMessageTextCreator(newMessageText)
+            // let newMessageText = values.message
+            // props.updatenewMessageTextCreator(newMessageText)
         }
     })
-    
+
+    const onChange = (e) => {
+        
+        formik.handleChange(e)
+        console.log(formik.values.message)
+        props.updatenewMessageTextCreator(formik.values.message)
+    }
+
+
     if (!props.isAuth) return <Redirect to={"/login"} />
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 {dialogsElements}
-             </div>
+            </div>
             <div className={s.messages}>
                 {messagesElements}
-
-                {/* <textarea ref={newMessageElement} onChange = {onNewMessageChange} value = {props.messagesPage.newMessageText}></textarea>
-                <div className={s.buttons}>
-                    <button onClick = {addMessage}>Add post</button>
-                    <button>remove</button>
-                </div>        */}
-                Formik
-                <FormikProvider  value={formik}>
-                {({ isValid, isSubmitting, values }) => (
-                <Form onSubmit={formik.handleSubmit}>
-                    <label htmlFor='name'>Message</label>
-                    <Field type = 'text' id='message' name='message' onKeyUp={formik.handleChange}/>
+                <form onSubmit={formik.handleSubmit}>
+                    <input type = 'text' id='message' name='message' onChange={onChange} value={formik.values.message}/>
                     <button type = 'submit' onClick = {addMessage}>Add post</button>
                     <button type = 'button'>Remove</button>
-                </Form>)
-                </FormikProvider>
+            </form>
             </div>
+            
         </div>
     )
 }
--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,31 +1,68 @@
-import { Field, Form, Formik } from "formik";
+import { useFormik } from "formik";
 
 
+const Setting = () => {
 
-const Setting = (props) => {
-
-    const submit = (values, {setSubmitting}) => {
-        setSubmitting(false)
-    }
-
+    const formik = useFormik({
+      initialValues: {
+        firstName: '',
+        lastName: '',
+      },
+      onSubmit: values => {
+        console.log(values)
+      },
+    })
     return (
-            <div>
-                Setting
-                <Formik
-                    initialValues={{ term: '' }}
-                    onSubmit={submit}
-                    >
-                    {({ isSubmitting }) => (
-                        <Form>
-                        <Field type="text" name="term" />
-                        <button type="submit" disabled={isSubmitting}>
-                            Submit
-                        </button>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+  
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+  
+        <button type="submit">Submit</button>
+      </form>
     )
-}
+  }
+
+// const Setting = (props) => {
+
+//     const submit = (values, {setSubmitting}) => {
+//         setSubmitting(false)
+//     }
+
+//     return (
+//             <div>
+//                 Setting
+//                 <Formik
+//                     initialValues={{ term: '' }}
+//                     onSubmit={submit}
+//                     >
+//                     {({ isSubmitting }) => (
+//                         <Form>
+//                         <Field type="text" name="term" />
+//                         <button type="submit" disabled={isSubmitting}>
+//                             Submit
+//                         </button>
+//                         </Form>
+//                     )}
+//                 </Formik>
+//             </div>
+//     )
+// }
+
+
 
 export default Setting

c08851ceda209db69d2e0efc79d8851086857fcc
bitewolf <2189108@gmail.com>
2021-06-07 21:47:19 +0300

    87

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,71 @@
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
-import React from 'react'
+import React, { PureComponent } from 'react'
 import validate from './../../../utils/validators/validatorsJax'
-import { useFormik } from 'formik'
+import { Formik, useFormik } from 'formik'
 
-const MyPosts = (props) => {
-    
+// class MyPosts extends PureComponent {
+
+//     // shouldComponentUpdate (nextProps, nextState) {
+//     //     return nextProps !== this.props || nextState !== this.state
+//     // }
+
+//     render() {
+//         console.log('Render')
+//         let postsElements = this.props.postData.map ( p => <Post message={p.message} likesCount={p.likesCount} key={p.id}/>)
+
+//         const initialValues = {
+//             postText: 'new post'
+//         }
+
+//         const onSubmit = values => {
+//             this.props.addPost(values.postText)
+//         }
+
+//         const formikProps = {
+//             initialValues,
+//             onSubmit,
+//             validate
+//         }
+
+
+//         return (
+//                 <div>
+//                     My posts
+//                     <div>
+//                         <Formik {...formikProps} >
+//                             {props => 
+//                                 (
+//                                     <form onSubmit={props.handleSubmit}>
+//                                     <div>
+//                                         <input type = 'text' id='postText' name='postText' onChange={props.handleChange} value={props.values.postText}/>
+//                                         {props.errors.postText ? <div className={s.errors}>{props.errors.postText}</div> : null}
+//                                     </div>
+//                                     <div className={s.buttons}>
+//                                         <button type = 'submit'>Add post</button>
+//                                         <button type = 'button'>Remove</button>
+//                                     </div>
+//                                 </form> 
+//                                 )}
+
+//                         </Formik>
+
+                        
+//                     </div>
+//                     <div className={s.posts}>
+//                         {postsElements}
+//                     </div>  
+//                 </div>
+//             )
+//     }
+
+
+// }
+
+
+const MyPosts = React.memo((props) => {
+
+console.log('Render')
 let postsElements = props.postData.map ( p => <Post message={p.message} likesCount={p.likesCount} key={p.id}/>)
 
 const initialValues = {
@@ -43,6 +103,6 @@ const formik = useFormik({
             </div>  
         </div>
     )
-}
+})
 
 export default MyPosts

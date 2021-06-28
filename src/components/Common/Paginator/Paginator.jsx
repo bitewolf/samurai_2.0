cb1404e0e437db68223d527c38df2ad79146e290
bitewolf <2189108@gmail.com>
2021-06-28 23:39:27 +0300

    paginator

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -1,32 +1,26 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styles from './paginator.module.css'
 
-let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
     let pagesCount = Math.ceil(totalUsersCount / pageSize)
     let pages = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
     
-    let portionCount = Math.ceil(pagesCount / portionSize)
-    let [portionNumber, setPortionNumber] = useState(1)
-    let leftPortionPageNumber = (portionNumber-1)*portionSize+1
-    let rightPortionPageNumber = portionNumber*portionSize
-    debugger
-
-
-
-
     return <div className={styles.paginator}>
-                {<button>&lt;&lt;</button>}
-                {portionNumber > 1 && <button onClick={()=>{setPortionNumber(portionNumber-1)}}>&lt;</button>}
+            {currentPage > 5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(1)}}>&lt;&lt;</button>}
+            {currentPage > 5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(currentPage-5)}}>&lt;</button>}
 
-                {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber).map((p) => {
-                   return <span key={p} className={currentPage === p && styles.selectedPage} onClick={(e) => {onPageChanged(p)}}>{p}</span>
-                })}
-                
-                {portionCount > portionNumber && <button onClick={()=>{setPortionNumber(portionNumber+1)}}>&gt;</button>}
-                {<button>&gt;&gt;</button>}
+            {pages.map(p => {
+            if (p === currentPage || (p < currentPage + 5 &&  p > currentPage - 5)) {
+                return <span className={currentPage === p && styles.selectedPage} onClick={(e) => { onPageChanged(p) }}>{p} </span>
+            }
+            
+        })}
+            
+            {currentPage < pagesCount-5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(currentPage+5)}}>&gt;</button>}
+            {currentPage < pagesCount-5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(pagesCount)}}>&gt;&gt;</button>}
             </div>
 }
 export default Paginator

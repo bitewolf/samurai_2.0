9691a5c6d9ae1a46cffd53d3812818e1c69f650d
bitewolf <2189108@gmail.com>
2021-06-30 17:13:28 +0300

    paginator+

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -11,14 +11,10 @@ let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
     return <div className={styles.paginator}>
             {currentPage > 5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(1)}}>&lt;&lt;</button>}
             {currentPage > 5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(currentPage-5)}}>&lt;</button>}
-
             {pages.map(p => {
             if (p === currentPage || (p < currentPage + 5 &&  p > currentPage - 5)) {
                 return <span className={currentPage === p && styles.selectedPage} onClick={(e) => { onPageChanged(p) }}>{p} </span>
-            }
-            
-        })}
-            
+            }})} 
             {currentPage < pagesCount-5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(currentPage+5)}}>&gt;</button>}
             {currentPage < pagesCount-5 && <button className={styles.paginatorButtons} onClick={(e)=>{onPageChanged(pagesCount)}}>&gt;&gt;</button>}
             </div>
